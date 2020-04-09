23806401da6c72f3520aae320c728f964455b52f
zyr <yanrong__zhang@163.com>
2020-04-09 21:19:07 +0800

    first commit

---
+++ jump7.py
@@ -0,0 +1,12 @@
+for a in range(1,101):
+    b=a%10
+    c=a%7
+    if b==7:
+        continue
+    elif c==0:
+        continue
+    elif a in range(71,80):
+    # elif a//10==7:
+        continue
+    else:
+        print(a)
